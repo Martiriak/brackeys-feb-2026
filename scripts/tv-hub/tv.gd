b0157420c62575226af5eafb4910bece28f4ac4d
Carlo Berlingeri <carlo.berlingeri@utg.lan>
2026-02-19 19:48:34 +0100

    Keepable object

--- scripts/tv-hub/tv.gd
+++ scripts/tv-hub/tv.gd
@@ -5,6 +5,7 @@ extends InteractableObject
 @onready var sub_viewport: SubViewport = $Sprite3D/SubViewport
 @onready var tv_screen: TvScreen = $Sprite3D/SubViewport/TvScreen
 
+var keepable_object = []
 
 func _unhandled_input(event: InputEvent) -> void:
 	sub_viewport.push_input(event)
