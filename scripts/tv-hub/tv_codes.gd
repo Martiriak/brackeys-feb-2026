ae7788156e7a60956881e6b8d33a6b0f041d6af8
Martiriak <dominiquepegoraro@gmail.com>
2026-02-19 22:23:41 +0100

    Added accessor functions to tv_codes.

--- scripts/tv-hub/tv_codes.gd
+++ scripts/tv-hub/tv_codes.gd
@@ -4,3 +4,11 @@ extends Resource
 
 @export
 var codes_to_maps: Dictionary[String, PackedScene]
+
+
+func verify_code(code: String) -> bool:
+	return code in codes_to_maps
+
+
+func get_map_with_code(code: String) -> PackedScene:
+	return codes_to_maps.get(code)
