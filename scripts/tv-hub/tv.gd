0f4c5d4bd68b413378b40c59ce22aa43469ca52d
Martiriak <dominiquepegoraro@gmail.com>
2026-02-19 19:22:07 +0100

    Added camera to Tv, interacting with TV changes camera.

--- scripts/tv-hub/tv.gd
+++ scripts/tv-hub/tv.gd
@@ -7,6 +7,7 @@ extends InteractableObject
 
 @onready var sub_viewport: SubViewport = $Sprite3D/SubViewport
 @onready var tv_screen: TvScreen = $Sprite3D/SubViewport/TvScreen
+@onready var tv_camera: Camera3D = $TvCamera
 
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -20,4 +21,9 @@ func _on_code_accepted(code: String) -> void:
 
 
 func on_interaction(p: Player) -> void:
-	print(p.name+" Interacted!")
+	var children_cameras: Array[Node] = p.find_children("*", "Camera3D")
+	if not children_cameras.is_empty():
+		var camera := children_cameras[0] as Camera3D
+		if is_instance_valid(camera):
+			camera.current = false
+			tv_camera.current = true
