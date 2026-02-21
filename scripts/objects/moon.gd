281072bffd1b33091f0fc7b1ebddcdd102b1c65b
dcaschili <danielecaschili@tiscali.it>
2026-02-21 23:57:20 +0100

    show end game code

--- scripts/objects/moon.gd
+++ scripts/objects/moon.gd
@@ -9,8 +9,23 @@ extends InteractableObject
 @onready var moon_position_1: Marker3D = $"../MoonPosition1"
 @onready var moon_position_2: Marker3D = $"../MoonPosition2"
 
+@export var levelCode : Array[int] = [0, 2, 0, 2, 0]
+
+@onready var sprite_3d: Sprite3D = $Sprite3D
+@onready var tv_screen: TvScreen = $Sprite3D/SubViewport/TvScreen
 
 var bLocked : bool = true
+
+	
+func _ready() -> void:
+	tv_screen.set_code(levelCode)
+
+func show_code():
+	sprite_3d.show()
+	tv_screen.hide_active_slot_indicator()
+
+func hide_code():
+	sprite_3d.hide()
 
 func _process(delta: float) -> void:
 	if GameManager.player_ref.inventoryItemsDict.has(requested_id):
@@ -27,6 +42,7 @@ func on_interaction(p: Player):
 			p.inventoryItemsDict[requested_id] -= requested_items_to_unlock
 			if p.inventoryItemsDict[requested_id] < 0:
 				p.inventoryItemsDict[requested_id] = 0
+			show_code()
 			bLocked = false
 
 func can_interact(p: Player):
