53627ebf3ea7e54a4c25b7a9e71b3fb68a00ab02
Daniele <48168349+ElDanee@users.noreply.github.com>
2026-02-21 17:53:06 +0100

    TV and player socket gas station

--- scripts/levels/gas_station.gd
+++ scripts/levels/gas_station.gd
@@ -1,3 +1,14 @@
 extends Node3D
 
 @onready var gas_station_pump := $Gas_Station_pump as Node3D
+var player : Player
+var tv : TV
+
+func _ready() -> void:
+	player = GameManager.player_ref
+	player.reparent(self)
+	player.transform = $PlayerSocket.transform
+	
+	tv = GameManager.tv_ref
+	tv.reparent(self)
+	tv.transform = $TVSocket.transform
