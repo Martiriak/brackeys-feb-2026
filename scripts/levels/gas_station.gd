18265fe11717b2ee7e2a142848a84ef6d56b5b38
dcaschili <danielecaschili@tiscali.it>
2026-02-21 18:17:05 +0100

    Update gas_station.gd

--- scripts/levels/gas_station.gd
+++ scripts/levels/gas_station.gd
@@ -6,9 +6,7 @@ var tv : TV
 
 func _ready() -> void:
 	player = GameManager.player_ref
-	player.reparent(self)
-	player.transform = $PlayerSocket.transform
+	player.transform = $PlayerSocket.transform	
 	
 	tv = GameManager.tv_ref
-	tv.reparent(self)
 	tv.transform = $TVSocket.transform
