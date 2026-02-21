323291edc61e4c7a18daae181d554bdc70040b71
VarXael <59068560+VarXael@users.noreply.github.com>
2026-02-21 17:11:50 +0100

    ADD: TV Sound effects

--- scripts/tv-hub/tv.gd
+++ scripts/tv-hub/tv.gd
@@ -3,12 +3,18 @@ extends InteractableObject
 
 
 @export var tv_codes: TvCodes = preload("res://levels/tv-hub/tv_codes.tres")
-
+@export var sound_accept: AudioStream = preload("res://SFX/TV_Accept.wav")
+@export var sound_error: AudioStream  = preload("res://SFX/TV_Error.wav")
+@export var sound_tv_ambience: AudioStream  = preload("res://SFX/TV_Ambience.wav")
+@export var sound_tv_interact: AudioStream  = preload("res://SFX/TV_Interact.wav")
 
 @onready var sub_viewport: SubViewport = $Sprite3D/SubViewport
 @onready var tv_screen: TvScreen = $Sprite3D/SubViewport/TvScreen
 @onready var tv_camera: Camera3D = $TvCamera
 @onready var tv_static_effect: MeshInstance3D = $ScreenStaticEffect
+
+@onready var sfx_player: AudioStreamPlayer3D = $sfx_player
+@onready var ambience_player: AudioStreamPlayer3D = $ambience_player
 
 var keepable_object = []
 
@@ -17,7 +23,18 @@ var _locked_player: Player = null
 
 #logic to avoid to detect the interact soon but only after the release
 var first_time: bool = true
+
+
+func _ready():
+	ambience_player.stream = sound_tv_ambience
+	ambience_player.play()
 	
+	# Creates a slow, eerie pitch wobble
+	var tween = create_tween().set_loops()
+	tween.tween_property(ambience_player, "pitch_scale", 0.75, 3.0).set_trans(Tween.TRANS_SINE)
+	tween.tween_property(ambience_player, "pitch_scale", 0.85, 3.0).set_trans(Tween.TRANS_SINE)
+
+
 func _unhandled_input(event: InputEvent) -> void:
 	sub_viewport.push_input(event)
 	
@@ -35,6 +52,12 @@ func _on_code_accepted(code: String) -> void:
 	print(code)
 	if level:
 		GameManager.load_new_level(level)
+		sfx_player.stream = sound_accept
+		sfx_player.play()
+	else:
+		sfx_player.stream = sound_error
+		sfx_player.play()
+	
 	
 	tv_screen.animate_shader(true if level else false, tv_static_effect)
 
@@ -42,6 +65,8 @@ func _on_code_accepted(code: String) -> void:
 func _exit_terminal():
 	if is_instance_valid(_locked_player):
 		_locked_player.unlock_play()
+		sfx_player.stream = null
+		ambience_player.play()
 		var children_cameras: Array[Node] = _locked_player.find_children("*", "Camera3D")
 		if not children_cameras.is_empty():
 			var camera := children_cameras[0] as Camera3D
@@ -54,6 +79,9 @@ func _exit_terminal():
 func on_interaction(p: Player) -> void:
 	_locked_player = p
 	p.lock_play()
+	ambience_player.stop()
+	sfx_player.stream = sound_tv_interact
+	sfx_player.play()
 	var children_cameras: Array[Node] = p.find_children("*", "Camera3D")
 	if not children_cameras.is_empty():
 		var camera := children_cameras[0] as Camera3D
